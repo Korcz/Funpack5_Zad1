7caf12a365851b3c52db109e0026195287b3ff9b
Kuba <Kuba@LAPTOP-66PQLDGC>
2016-07-20 14:11:27 +0200

    DONE

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,58 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main
 {
-
+	
+	
 	public static void main(String[] args)
 	{
+		double MAXw = 10;
 		
-
+		List<Product> lista = new ArrayList<>();
+		
+		lista.add(new Product("Shirt", 75, 7));
+		lista.add(new Product("Trouers", 150, 8));
+		lista.add(new Product("Sweater", 250, 6));
+		lista.add(new Product("Cap", 35, 4));
+		lista.add(new Product("Trunks", 10, 3));
+		lista.add(new Product("Footwear", 100, 9));
+		
+		Collections.sort(lista, new Comparator<Product>()
+		{
+			public int compare(Product p1, Product p2)
+			{
+				if(p1.getCount() > p2.getCount()) return -1;
+				if(p1.getCount() < p2.getCount()) return 1;
+				return 0;
+			}
+		});
+		
+		for (int i = 0; i < lista.size(); i++)
+		{
+			System.out.println(lista.get(i).toString());
+		}
+		
+		List<Product> backPack = new ArrayList<>();
+		
+		for (int i = 0; i < lista.size(); i++)
+		{
+			if(lista.get(i).getWeight() <= MAXw && MAXw != 0)
+			{
+					MAXw = MAXw - lista.get(i).getWeight();
+					backPack.add(lista.get(i));
+			}
+		}
+		
+		System.out.println();
+		System.out.println("----------------------------------BACKPAC---------------------------------------");
+		
+		for (Product produkt : backPack)
+		{
+			System.out.println(produkt);
+		}
+		
 	}
-
 }
--- src/Product.java
+++ src/Product.java
@@ -5,31 +5,109 @@ public class Product
 	private String name;
 	private double price;
 	private double weight;
-	
+	private double value = price/weight;
+
+	public Product(String aName, double aPrice, double aWeight)
+	{
+		this.name = aName;
+		this.price = aPrice;
+		this.weight = aWeight;
+		this.value = aPrice/aWeight;
+	}
+
 	public Product()
 	{
 		System.out.println("Enter prouct name");
 		Scanner sc = new Scanner(System.in);
-		
+		name = sc.nextLine();
 		System.out.println("Enter price");
+		price = Double.parseDouble(sc.nextLine());
+		weight = Double.parseDouble(sc.nextLine());
 	}
 	
+	
+
+	public double getCount()
+	{
+		return value;
+	}
+
+	public void setCount(double count)
+	{
+		this.value = count;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
 	public double getPrice()
 	{
 		return price;
 	}
+
 	public void setPrice(double price)
 	{
 		this.price = price;
 	}
+
 	public double getWeight()
 	{
 		return weight;
 	}
+
 	public void setWeight(double weight)
 	{
 		this.weight = weight;
 	}
-	
-	
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(weight);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (name == null)
+		{
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		if (Double.doubleToLongBits(weight) != Double.doubleToLongBits(other.weight))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Product [Name = " + name + ", price = " + price + ", weight = " + weight + ", value =  " + value + "]";
+	}
+
 }
